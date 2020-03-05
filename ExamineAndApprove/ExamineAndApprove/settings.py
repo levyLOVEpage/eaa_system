48266aaa864cf5534033689f2d53272185ecc3e5
levy <greatfqc@gamil.com>
2020-03-05 16:10:32 +0800

    修改debug模式

--- ExamineAndApprove/ExamineAndApprove/settings.py
+++ ExamineAndApprove/ExamineAndApprove/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!y_hdn-*b_i19g=u7&j!4)c5mnz%q6&*k)tsf^dk1x8)^g!4b%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
