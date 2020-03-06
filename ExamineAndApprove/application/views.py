0923e616ffcd5c110efd993a83729af797d97540
levy <greatfqc@gamil.com>
2020-03-06 15:36:29 +0800

    新增提交申请

--- ExamineAndApprove/application/views.py
+++ ExamineAndApprove/application/views.py
@@ -9,13 +9,20 @@ from common.extensions.auth import JwtAuthentication,create_token
 from common.code import code
 class Apply(APIView):
     authentication_classes = []
-    def get(self,applicant_id,format=None):
-        queryset = ApplicantList.objects.all(applicant_id=applicant_id)
-        s = serializers.ApplySerializer(queryset)
+    def get(self,request,applicant_id,format=None):
+        queryset = ApplicantList.objects.filter(applicant_id=applicant_id)
+        s = serializers.ApplySerializer(queryset,many=True)
         return Response({'status':
                              {'code':code.success_code[0],'msg':code.success_code[1]},
                          'data':s.data
                          })
+    def post(self,request,format=None):
+        s = serializers.ApplySerializer(data=request.data)
+        if s.is_valid():
+            s.save()
+            return({'status':{'code':code.success_code[0],'msg':code.success_code[1]}})
+        else:
+            return({'status':{'code':code.error_2004[0],'msg':code.error_2004[1]}})
 
 
 
