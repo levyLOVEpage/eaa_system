82351aa19cdbb4a3816a8ab8b8353f933b009b49
levy <greatfqc@gamil.com>
2020-03-08 11:29:54 +0800

    增加分页器

--- ExamineAndApprove/ExamineAndApprove/settings.py
+++ ExamineAndApprove/ExamineAndApprove/settings.py
@@ -55,9 +55,10 @@ MIDDLEWARE = [
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    '*'
-)
+# CORS_ORIGIN_WHITELIST = (
+#     '*',
+#     'http://127.0.0.1:8000'
+# )
 
 CORS_ALLOW_METHODS = (
     'DELETE',
--- ExamineAndApprove/application/views.py
+++ ExamineAndApprove/application/views.py
@@ -7,15 +7,22 @@ import jwt
 import datetime
 from common.extensions.auth import JwtAuthentication,create_token
 from common.code import code
+from common.extensions.pagination import LimitOffset
 class ApplyList(APIView):
     authentication_classes = []
     def get(self,request,applicant_id,format=None):
         queryset = ApplicantList.objects.filter(applicant_id=applicant_id)
         s = serializers.ApplySerializer(queryset,many=True)
-        return Response({'status':
+
+        page_obj = LimitOffset()
+
+        apply_list ={'status':
                              {'code':code.success_code[0],'msg':code.success_code[1]},
                          'data':s.data
-                         })
+                         }
+        page_list = page_obj.paginate_queryset(queryset=queryset,request=request, view=self)
+        s = serializers.ApplySerializer(page_list,many=True)
+        return Response(s.data)
 class Apply(APIView):
     authentication_classes = []
     def post(self,request,format=None):
