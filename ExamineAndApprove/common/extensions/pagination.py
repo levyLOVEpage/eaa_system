9ee25745d0c0bb271116d02a77950487735d1028
levy <greatfqc@gamil.com>
2020-04-02 18:10:28 +0800

    新增功能

--- ExamineAndApprove/common/extensions/pagination.py
+++ ExamineAndApprove/common/extensions/pagination.py
@@ -1,6 +1,6 @@
 from rest_framework.pagination import LimitOffsetPagination
 class LimitOffset(LimitOffsetPagination):
-    default_limit = 5
+    default_limit = 50
     limit_query_param = 'limit'
     offset_query_param = 'offset'
     max_limit = 99
